a12f98947a8876815e8e8991bae812fa5617e6f3
Alejandro <alejandro.cornejo23@gmail.com>
2023-05-05 10:03:37 +0400

    fine tuning

--- damage_plot/dam_plot.py
+++ damage_plot/dam_plot.py
@@ -5,8 +5,6 @@ import numpy
 def ComputeDamage(UniaxialStress, Threshold, Gf, L, Young, Softening):
     if (Softening ==  "Linear"):
         A = ComputeAParameterLinear(Threshold, Gf, L, Young)
-        if A <= 0.0:
-            raise ValueError("A negative")
         return ComputeDamageLinear(UniaxialStress, Threshold, A)
     else: # Exponential
         A = ComputeAParameterExponential(Threshold, Gf, L, Young)
@@ -14,9 +12,17 @@ def ComputeDamage(UniaxialStress, Threshold, Gf, L, Young, Softening):
             raise ValueError("A negative")
         return ComputeDamageExponential(UniaxialStress, Threshold, A)
 def ComputeDamageLinear(UniaxialStress, Threshold, A):
-    return (1.0 - Threshold / UniaxialStress) / (1.0 + A)
+    d = (1.0 - Threshold / UniaxialStress) / (1.0 + A)
+    if d >= 0.999:
+        return 0.999
+    else:
+        return d
 def ComputeDamageExponential(UniaxialStress, Threshold, A):
-    return 1.0 - (Threshold / UniaxialStress) * math.exp(A * (1.0 - UniaxialStress / Threshold))
+    d = 1.0 - (Threshold / UniaxialStress) * math.exp(A * (1.0 - UniaxialStress / Threshold))
+    if d >= 0.999:
+        return 0.999
+    else:
+        return d
 def ComputeAParameterLinear(Threshold, Gf, L, Young):
     return -Threshold**2 / (2.0 * Young * Gf / L)
 def ComputeAParameterExponential(Threshold, Gf, L, Young):
@@ -27,7 +33,7 @@ E = 2.1e9      # Pa
 l_char = 0.1   # m
 fy = 500e6     # Pa
 Gf = 15000000      # J/m2
-softening = "Exponential"
+softening = "Linear"
 
 initial_strain = 0.0
 final_strain   = fy/E*3.0
@@ -50,6 +56,6 @@ plt.plot(strain, integrated_stress, "r", label = "Damage response", linewidth=2)
 plt.xlabel('Strain [-]', fontsize = 12)
 plt.ylabel('Stress [Pa]', fontsize = 12)
 plt.legend(fontsize = 13)
-plt.title('Damage case: ' + "E = " + str(E) + "\n" + "Yield: " + str(fy) + "\n", fontsize = 12)
+plt.title('Damage case: ' + "E = " + str(E) + "\n" + "Yield: " + str(fy) + "\n" + "Gf = " + str(Gf) + "\n" + "L_char = " + str(l_char), fontsize = 12)
 plt.grid()
 plt.show()
