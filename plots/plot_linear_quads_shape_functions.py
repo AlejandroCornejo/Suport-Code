0a757b6b74890c61d37dad2415a6827ad3422f17
Alejandro <alejandro.cornejo23@gmail.com>
2023-08-19 13:07:39 +0400

    .

---
+++ plots/plot_linear_quads_shape_functions.py
@@ -0,0 +1,58 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+'''
+             v
+             ^
+             |
+       4-----------3
+       |     |     |
+       |     |     |
+       |     +---- | --> u
+       |           |
+       |           |
+       1-----------2
+ '''
+ 
+#################
+shape_function_node = 4
+#################
+
+# We define the shape functions
+def N(xi, eta, node):
+    if node == 1:
+        return 0.25*(1.0-xi)*(1.0-eta)
+    elif node == 2:
+        return 0.25*(1.0+xi)*(1.0-eta)
+    elif node == 3:
+        return 0.25*(1.0+xi)*(1.0+eta)
+    elif node == 4:
+        return 0.25*(1.0-xi)*(1.0+eta)
+
+# Generate x and y values
+x = np.linspace(-1, 1, 100)
+y = np.linspace(-1, 1, 100)
+X, Y = np.meshgrid(x, y)
+
+# Calculate the function values for each (x, y) pair
+Z = N(X, Y, shape_function_node)
+
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the surface
+surf = ax.plot_surface(X, Y, Z, cmap='viridis')
+
+# Add labels and title
+ax.set_xlabel('xi')
+ax.set_ylabel('eta')
+ax.set_zlabel('N(xi.eta)')
+ax.set_title('Shape function')
+
+# Add colorbar
+fig.colorbar(surf, ax=ax)
+
+# Show the plot
+plt.show()
---
+++ plots/plot_quadratic_quads_shape_functions.py
@@ -0,0 +1,62 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+'''
+3-----6-----2
+|           |
+|           |
+7     8     5
+|           |
+|           |
+0-----4-----1
+ '''
+ 
+#################
+shape_function_node = 0
+#################
+
+# We define the shape functions
+def N(xi, eta, node):
+    fx1 = 0.5 * ( xi - 1 ) * xi;
+    fx2 = 0.5 * ( xi + 1 ) * xi;
+    fx3 = 1 - xi * xi;
+    fy1 = 0.5 * ( eta - 1 ) * eta;
+    fy2 = 0.5 * ( eta + 1 ) * eta;
+    fy3 = 1 - eta * eta;
+    if node == 0:
+        return fx1*fy1
+    elif node == 1:
+        return fx2*fy1
+    elif node == 2:
+        return fx2*fy2
+    elif node == 3:
+        return fx1*fy2
+    # ...
+
+# Generate x and y values
+x = np.linspace(-1, 1, 100)
+y = np.linspace(-1, 1, 100)
+X, Y = np.meshgrid(x, y)
+
+# Calculate the function values for each (x, y) pair
+Z = N(X, Y, shape_function_node)
+
+# Create a 3D plot
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot the surface
+surf = ax.plot_surface(X, Y, Z, cmap='viridis')
+
+# Add labels and title
+ax.set_xlabel('xi')
+ax.set_ylabel('eta')
+ax.set_zlabel('N(xi.eta)')
+ax.set_title('Shape function')
+
+# Add colorbar
+fig.colorbar(surf, ax=ax)
+
+# Show the plot
+plt.show()
